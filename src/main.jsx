64f809f26d837d39c50a683aceb4f3d56a8076dd
Daniela Maica <danielamaica43@gmail.com>
2025-11-03 12:34:16 +0000

    Resolviendo conflicto después de stash pop

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ const Main = () => {
             {/* Provide global state to all components */}
             <StoreProvider> 
                 {/* Set up routing for the application */} 
-                <RouterProvider router={router}>
+                <RouterProvider>
                 </RouterProvider>
             </StoreProvider>
         </React.StrictMode>
